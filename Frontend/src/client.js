e304bf855de474ba46d7c5e20f04960976cab6be
MTeeeee <mete.ardic@gmail.com>
2023-02-27 19:25:50 +0100

    create Answer Buttons on GET and delete 4 Answer Buttons on Next Question

--- Frontend/src/client.js
+++ Frontend/src/client.js
@@ -1,21 +1,50 @@
 // #################### new Question / GET #############################################
 
+let random_array = ["a","b","c","d"];
+
 function getData() {
   fetch("http://localhost:4000/question/random")
     .then((response) => response.json())
-    .then((json) => fill_text_area(json))
+    .then((json) => create_buttons(json))
     .catch((error) => console.log(error));
 
-  function fill_text_area(quiz_data) {
 
-    current_id = quiz_data.id;
 
-    document.getElementById("question").innerHTML = quiz_data.question;
-    document.getElementById("answer_1").innerHTML = quiz_data.answers[0].text;
-    document.getElementById("answer_2").innerHTML = quiz_data.answers[1].text;
-    document.getElementById("answer_3").innerHTML = quiz_data.answers[2].text;
-    document.getElementById("answer_4").innerHTML = quiz_data.answers[3].text;
-  }
+    function create_buttons(quiz_data){
+      console.log(quiz_data);
+      
+      current_id = quiz_data.id;
+
+      document.getElementById("question").innerHTML = quiz_data.question;
+
+      const container = document.getElementById("buttons");
+      quiz_data.answers.forEach((answer, i) => 
+      {
+        const button = document.createElement('button');
+        button.id = "answer_" + (i + 1);
+        button.addEventListener("click", function bla(id) {check_answer_backend(id)})
+        console.log(answer.text)
+        button.textContent = answer.text;
+        container.appendChild(button);
+      })}
+
+      // array.forEach((answer, i) => 
+      // {
+      //   const button = document.createElement('button');
+      //   button.textContent = answer;
+      //   container.appendChild(button);
+      // })}   
+
+  // function fill_text_area(quiz_data) {
+
+  //   current_id = quiz_data.id;
+
+  //   document.getElementById("question").innerHTML = quiz_data.question;
+  //   // document.getElementById("answer_1").innerHTML = quiz_data.answers[0].text;
+  //   // document.getElementById("answer_2").innerHTML = quiz_data.answers[1].text;
+  //   // document.getElementById("answer_3").innerHTML = quiz_data.answers[2].text;
+  //   // document.getElementById("answer_4").innerHTML = quiz_data.answers[3].text;
+  // }
 }
 getData();
 
@@ -44,6 +73,8 @@ function check_answer_backend(id) {
     res.forEach((answer) => {
       for (let i = 1; i < res.length + 1; i++) {
       const index_id = "answer_" + i;
+      console.log(answer.tex)
+      console.log(document.getElementById(index_id).innerHTML)
       if (String(answer.text) === document.getElementById(index_id).innerHTML) {
         if (answer.isCorrect === true) {
           document.getElementById(index_id).style.backgroundColor = "green";
@@ -64,9 +95,9 @@ function load_new_question_and_reset_colors() {
 }
 
 
-function resetAnswers() {
-  document.getElementById("answer_1").style.backgroundColor = "";
-  document.getElementById("answer_2").style.backgroundColor = "";
-  document.getElementById("answer_3").style.backgroundColor = "";
-  document.getElementById("answer_4").style.backgroundColor = "";
+function resetAnswers() { // bitte iterieren
+  document.getElementById("answer_1").remove();
+  document.getElementById("answer_2").remove();
+  document.getElementById("answer_3").remove();
+  document.getElementById("answer_4").remove();
 }
