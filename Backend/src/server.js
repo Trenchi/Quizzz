d9ea208365896d8e9e10806147472a2b17f68f37
MTeeeee <mete.ardic@gmail.com>
2023-03-01 09:27:58 +0100

    done

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const { getRandomQuestion } = require("./controller/questions");
+const { getRandomQuestion, postRandomQuestion } = require("./controller/questions");
 const { checkAnswer } = require("./controller/answers");
 const { connectToDatabase, connectAndQuery } = require('./services/database');
+
 
 const app = express();
 const port = 4000;
@@ -10,33 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 
-
 app.get("/question/random", getRandomQuestion);
+app.post("/question/random", postRandomQuestion)
 app.post('/question/check_answer', checkAnswer);
 
-// async (req, res) => {
-//   const id = req.params.id;
-//   const query = 'SELECT * FROM quizzz WHERE id = $1';
-//   const params = [id];
-  
-//   const result = await connectAndQuery(query, params);
-
-//   res.send(result);
-// };
-
-
-
-
-
-// app.get('/quiz/:id', async (req, res) => {
-//   const id = req.params.id;
-//   const query = 'SELECT * FROM quizzz WHERE id = $1';
-//   const params = [id];
-  
-//   const result = await connectAndQuery(query, params);
-
-//   res.send(result);
-// });
 
 connectToDatabase().then(() => {
   app.listen(port, () => {
