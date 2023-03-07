378051e99b54d41a9d85722d6054bc8195c6271d
MTeeeee <mete.ardic@gmail.com>
2023-03-07 14:05:59 +0100

    Work on Backend / New Folder structure Frontend(Maik)

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const {  postRandomQuestion } = require("./controller/questions");
 const { checkAnswer } = require("./controller/answers");
+const { getHighscore, postHighscore } = require("./controller/highscore");
 const { connectToDatabase, connectAndQuery } = require('./services/database');
 
 const app = express();
@@ -9,8 +10,10 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 
-app.post("/question/random", postRandomQuestion)
-app.post('/question/check_answer', checkAnswer);
+app.post("/question/random", postRandomQuestion);
+app.post("/question/check_answer", checkAnswer);
+app.get("/highscore", getHighscore);
+app.post("/highscore", postHighscore);
 
 connectToDatabase().then(() => {
   app.listen(port, () => {
